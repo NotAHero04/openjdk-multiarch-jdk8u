69c9050d58caaf84c870fb705bcc47b797771853
NotAHero04 <84689840+NotAHero04@users.noreply.github.com>
2022-04-23 16:27:22 +0700

    Update immediate_aarch64.cpp
    Fix JDK-8280476

--- hotspot/src/cpu/aarch64/vm/immediate_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/immediate_aarch64.cpp
@@ -122,8 +122,12 @@ static inline u_int32_t uimm(u_int32_t val, int hi, int lo)
 
 u_int64_t replicate(u_int64_t bits, int nbits, int count)
 {
+  // Special case nbits == 64 since the resulting shift below would result in undefined behavior
+  if (nbits == 64) {
+  assert(count <= 1);
+  return bits;
+  }
   u_int64_t result = 0;
-  // nbits may be 64 in which case we want mask to be -1
   u_int64_t mask = ones(nbits);
   for (int i = 0; i < count ; i++) {
     result <<= nbits;
